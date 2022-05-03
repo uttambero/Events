f66187dc3146483273266408baca68c24e55d039
Uttam kumar <uttam.1rn12ec168@gmail.com.com>
2022-05-03 13:18:33 +0530

    Add project files.

---
+++ Events/ThresholdReachedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Events
+{
+    public class ThresholdReachedEventArgs : EventArgs
+        {
+            public int Threshold { get; set; }
+            public DateTime TimeReached { get; set; }
+        }
+    }
+
+    
